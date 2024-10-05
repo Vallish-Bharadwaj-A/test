af7f1b084c0da58625dc079115de119d8c223195
Vallish-Bharadwaj-A <vallisha94483@gmail.com>
2024-10-06 01:02:23 +0530

    github url added

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ require("dotenv").config();
 
 const port = 4000;
 const app = express();
+const data = {
+  "type": "User",
+  "site_admin": false,
+  "name": null,
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 1,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 0,
+  "created_at": "2021-05-24T19:10:30Z",
+  "updated_at": "2024-10-05T18:57:59Z"
+};
 
 app.get("/", (req, res) => {
   res.send("Hello pappu");
@@ -10,6 +28,10 @@ app.get("/", (req, res) => {
 
 app.get("/login",(req, res) => {
   res.send("Hello login lowdu");
+})
+
+app.get("/github",(req,res)=>{
+  res.json(data);
 })
 
 app.listen(process.env.PORT, () => {
